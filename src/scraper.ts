2d10d5c735a7bf4f77ac105140c012b039587f12
colspan <collin@collindigm.com>
2024-08-18 00:14:14 +0300

    Scrape Web page function updated to save the response

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 import {ScrapeResult} from "./types";
+import { ScrapeResult as ScrapeResultModel } from './models/ScrapeResult';
 import * as cheerio from "cheerio";
 
 
-export async function scrapeWebPage(url: string): Promise<ScrapeResult | null> {
+export async function scrapeWebPage(url: string): Promise<void> {
     try {
         const {data} = await axios.get(url);
         const $ = cheerio.load(data);
@@ -11,9 +12,11 @@ export async function scrapeWebPage(url: string): Promise<ScrapeResult | null> {
         const title = $('head > title').text();
         const description = $('meta[name=description]').attr('content') || '';
 
-        return {title, description};
+        const scrapeResult = new ScrapeResultModel({url, title, description})
+        await scrapeResult.save();
+
+        console.log(`Saved: ${url}`);
     } catch (error) {
         console.error(`Error scraping ${url}:`, error);
-        return null;
     }
 }
