7e6b0a93005fe315d960031f29118ab1b2c2d555
colspan <collin@collindigm.com>
2024-08-18 01:43:06 +0300

    Connect to MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import {scrapeWebPage} from "./scraper";
+import * as mongoose from "mongoose";
+
+const mongoUri = 'mongodb://webscribe:password@localhost:27017/webscraper?authSource=admin';
 
 const url: string = "https://www.scrapethissite.com/pages/";
 
-scrapeWebPage(url).then((result) => {
-    if (result) {
-        console.log('Page Title:', result.title);
-        console.log('Description:', result.description);
-    } else {
-        console.log('Failed to scrape the webpage.');
-    }
+mongoose.connect(mongoUri)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err.message);
+    })
+
+scrapeWebPage(url).then(() => {
+    mongoose.connection.close();
 })
