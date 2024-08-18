261b51398dd0fd0641304d5cbb5d8d09d8448d0a
Lemirq <sharmavihaan190@gmail.com>
2024-08-18 12:37:19 -0400

    Updated actions UI, updated showing the loadInteractions

--- server/src/controllers/scraperController.ts
+++ server/src/controllers/scraperController.ts
@@ -3,29 +3,30 @@ import { scrapeAndInteract } from '../services/browserService';
 import { Interaction } from '../models/scrapedData';
 
 export const handleScrapeRequest = async (req: Request, res: Response) => {
-    const { url, actions } = req.body;
-    console.log(`Received scrape request for URL: ${url} with actions: ${JSON.stringify(actions)}`);
+	const { url, actions } = req.body;
+	console.log('ACTIONS', actions);
 
-    try {
-        if (!url || !Array.isArray(actions)) {
-            return res.status(400).json({ error: 'URL must be provided and actions must be an array.' });
-        }
+	console.log(`Received scrape request for URL: ${url} with actions: ${JSON.stringify(actions)}`);
 
-        const result = await scrapeAndInteract(url, actions);
-        res.status(200).json(result);
-    } catch (error) {
-        console.error(`Error in handleScrapeRequest: ${error || error}`);
-        res.status(500).json({ error: error });
-    }
+	try {
+		if (!url || !Array.isArray(actions)) {
+			return res.status(400).json({ error: 'URL must be provided and actions must be an array.' });
+		}
+
+		const result = await scrapeAndInteract(url, actions);
+		res.status(200).json(result);
+	} catch (error) {
+		console.error(`Error in handleScrapeRequest: ${error || error}`);
+		res.status(500).json({ error: error });
+	}
 };
 
-
 export const getInteractions = async (req: Request, res: Response) => {
-    try {
-        const interactions = await Interaction.find().sort({ timestamp: -1 });
-        res.status(200).json(interactions);
-    } catch (error) {
-        console.error(`Error retrieving interactions: ${error}`);
-        res.status(500).json({ error: error });
-    }
+	try {
+		const interactions = await Interaction.find().sort({ timestamp: -1 });
+		res.status(200).json(interactions);
+	} catch (error) {
+		console.error(`Error retrieving interactions: ${error}`);
+		res.status(500).json({ error: error });
+	}
 };
