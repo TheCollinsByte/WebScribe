945b381d14c0ebf7dd1b8edfac45248d2c963751
colspan <collin@collindigm.com>
2024-08-18 04:02:04 +0300

    Hanlde CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors';
 import * as mongoose from "mongoose";
 import scraperRouter from './routes/scraper';
 
@@ -6,6 +7,13 @@ const mongoUri = 'mongodb://webscribe:password@localhost:27017/webscraper?authSo
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+app.use(cors({
+    origin: 'http://localhost:1234',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 app.use(express.static('public'));
